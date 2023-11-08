82574e3ced0ec07be907effc4c25d2b2da2fe535
divyam gupta <divyam7091@gmail.com>
2023-11-09 01:53:52 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Feeds } from "@/components/Feeds";
 import { fetchFeeds } from "@/serveractions/fetchFeeds";
-import dynamic from "next/dynamic";
 export default async function Home() {
   const data = await fetchFeeds(0);
   return <Feeds initialCursor={data.cursor} initialPosts={data.data} />;
